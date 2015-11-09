4538874e791e7a25c9f5bc2ed6ef96e5533abddf
alexforsale <alexforsale@yahoo.com>
2015-11-09 17:09:40 +0700

    add tty0 as console

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_CPU_VARIANT := krait
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/armani
 TARGET_KERNEL_CONFIG := armani_ubuntu_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=armani user_debug=31 msm_rtb.filter=0x37
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=armani user_debug=31 msm_rtb.filter=0x37 console=tty0
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
